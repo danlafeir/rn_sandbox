6f1f2aa6313cc328a0f892d4dbf39d6148a5d7fa
Dan LaFeir <danlafeir@gmail.com>
2016-10-31 02:14:11 -0400

    Dan | Add endpoint stubs for /playlist

--- server.js
+++ server.js
@@ -10,3 +10,24 @@ app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true }) );
 
 app.listen(port, console.log(`server listening on ${port}`));
+
+const testData = [
+	{	title: "song1", artist: "artist1", uri: "uri1", votes: 0 },
+	{	title: "song2", artist: "artist2", uri: "uri2", votes: 0 },
+	{	title: "song3", artist: "artist3", uri: "uri3", votes: 0 },
+	{	title: "song4", artist: "artist4", uri: "uri4", votes: 0 },
+];
+
+app.post('/playlist/add', (req, res) => {
+	console.log(req.body);
+	res.send({ playlist: testData })
+});
+
+app.get('/playlist/songs', (req, res) => {
+	res.send({ playlist: testData })
+});
+
+app.post('playlist/vote', (req, res) => {
+	console.log(req.body);
+	res.send({ playlist: testData })
+});
